b231d3fe9797f660474a1fdd3a6f066e6489f58e
Ubuntu <ubuntu@ip-172-31-13-120.us-west-2.compute.internal>
2013-07-06 13:42:48 +0000

    Added iput sourcse

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var inputFile = fs.readFileSync('index.html').toString();
+console.log(inputFile);
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send('inputFile');
 });
 
 var port = process.env.PORT || 5000;
